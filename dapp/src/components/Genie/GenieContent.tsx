7fcc50d45e41c454219111011eaa615bb4a3ee0e
0xRomain <romain@talentlayer.org>
2023-11-18 13:41:57 +0300

    update openAi implementation

--- dapp/src/components/Genie/GenieContent.tsx
+++ dapp/src/components/Genie/GenieContent.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { FiArrowLeft, FiSend } from 'react-icons/fi';
+import { FiArrowLeft, FiLoader, FiSend } from 'react-icons/fi';
 import { Genie } from '../../types';
 import DropDataGenieModal from '../Modal/DropDataGenieModal';
+import { ThreadMessage, ThreadMessagesPage } from 'openai/resources/beta/threads/messages/messages';
 
 interface IMessage {
   sender: 'user' | 'assistant';
@@ -17,21 +18,17 @@ interface GenieContentProps {
 export default function GenieContent({ selectedGenie, onBack }: GenieContentProps) {
   const [userInput, setUserInput] = useState('');
 
-  const [conversation, setConversation] = useState<IMessage[]>([]);
-  const [threadId, setThreadId] = useState<string | null>(null);
+  const [conversation, setConversation] = useState<ThreadMessage[]>([]);
+  const [loading, setLoading] = useState(false);
 
   const [assistant, setAssistant] = useState<any>(null);
 
-  async function threadCreation() {
+  async function postMessage() {
+    setLoading(true);
     // Add user message to the conversation
-    setConversation(prevConvo => [...prevConvo, { sender: 'user', text: userInput }]);
+    // setConversation(prevConvo => [...prevConvo, { sender: 'user', text: userInput }]);
 
     const existingThreadId = localStorage.getItem('threadId');
-
-    if (existingThreadId) {
-      console.log('Thread ID already exists:', existingThreadId);
-      setThreadId(existingThreadId);
-    }
 
     try {
       const response = await fetch('/api/ai/create-message', {
@@ -43,31 +40,30 @@ export default function GenieContent({ selectedGenie, onBack }: GenieContentProp
         throw new Error('Network response was not ok');
       }
 
-      const thread = await response.json();
-      console.log('thread', thread);
+      const { threadId, messages } = await response.json();
+      console.log('response', { threadId, messages });
 
       // Assuming your API returns the assistant's response
-      if (thread && thread.assistantResponse) {
-        setConversation(prevConvo => [
-          ...prevConvo,
-          { sender: 'assistant', text: thread.assistantResponse },
-        ]);
+      if (messages) {
+        setConversation(messages.reverse());
       }
 
-      if (thread && thread.id) {
-        localStorage.setItem('threadId', thread.id);
-        console.log('Thread ID saved to local storage:', thread.id);
+      if (threadId) {
+        localStorage.setItem('threadId', threadId);
+        console.log('Thread ID saved to local storage:', threadId);
       }
     } catch (error) {
       console.error('Error creating the thread:', error);
+    } finally {
+      setLoading(false);
+      // Reset the input field
+      setUserInput('');
     }
-
-    // Reset the input field
-    setUserInput('');
   }
 
   return (
     <>
+      <div></div>
       <div className='flex-1 p-4'>
         <button onClick={onBack} className='mb-4 lg:hidden'>
           <FiArrowLeft size={24} />
@@ -84,8 +80,10 @@ export default function GenieContent({ selectedGenie, onBack }: GenieContentProp
         <div className='mt-4'>
           <div className='border p-2 h-64 overflow-auto'>
             {conversation.map((msg, index) => (
-              <div key={index} className={`message ${msg.sender === 'user' ? 'text-right' : ''}`}>
-                {msg.text}
+              <div key={index} className={`message ${msg.role === 'user' ? 'text-right' : ''}`}>
+                {msg.content.map((content, index) => (
+                  <p key={index}>{content.text?.value}</p>
+                ))}
               </div>
             ))}
           </div>
@@ -97,8 +95,8 @@ export default function GenieContent({ selectedGenie, onBack }: GenieContentProp
               className='border p-2 flex-grow'
               placeholder='Type your message here'
             />
-            <button onClick={threadCreation} className='ml-2 bg-blue-500 text-white p-2'>
-              <FiSend />
+            <button onClick={postMessage} className='ml-2 bg-blue-500 text-white p-2'>
+              {loading ? <FiLoader className='animate-spin' /> : <FiSend />}
             </button>
           </div>
         </div>
--- dapp/src/pages/api/ai/create-message.tsx
+++ dapp/src/pages/api/ai/create-message.tsx
@@ -3,20 +3,28 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 const openApiKey = process.env.NEXT_PRIVATE_OPENAI_API_KEY;
 
+export const sleep = (seconds: number) =>
+  new Promise<void>(resolve => {
+    setTimeout(() => resolve(), seconds * 1000);
+  });
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // json decode the request body
 
-  const { content, threadId } = JSON.parse(req.body);
+  let { content, threadId } = JSON.parse(req.body);
 
   // Check if API key is available
   if (!openApiKey) {
     return res.status(500).json({ error: 'API key is missing' });
   }
 
+  console.log({ content, threadId });
+
   try {
     const openai = new OpenAI({ apiKey: openApiKey });
 
     if (!threadId) {
+      console.log('creating thread');
       const thread = await openai.beta.threads.create({
         messages: [
           {
@@ -25,14 +33,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           },
         ],
       });
-      const run = await openai.beta.threads.runs.create(threadId, {
-        assistant_id: 'asst_MzDSgPrndkcyYkScrkaEXbSa',
-        model: 'gpt-4-1106-preview',
-        instructions: 'additional instructions',
-        tools: [{ type: 'code_interpreter' }, { type: 'retrieval' }],
-      });
 
-      res.status(200).json(thread);
+      console.log('thread created', { thread });
+
+      threadId = thread.id;
     }
 
     const threadMessages = await openai.beta.threads.messages.create(threadId, {
@@ -40,8 +44,24 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       content: content,
     });
 
-    // return the thread as a response
-    res.status(200).json(threadMessages);
+    let run = await openai.beta.threads.runs.create(threadId, {
+      assistant_id: 'asst_MzDSgPrndkcyYkScrkaEXbSa',
+      model: 'gpt-4-1106-preview',
+      instructions: 'additional instructions',
+      tools: [{ type: 'code_interpreter' }, { type: 'retrieval' }],
+    });
+
+    while (run.status === 'queued' || run.status === 'in_progress') {
+      await sleep(0.5);
+      run = await openai.beta.threads.runs.retrieve(threadId, run.id);
+      console.log(run.started_at, run.status);
+    }
+    console.log('run finished:', run.status, JSON.stringify(run, null, 2));
+
+    const messages = await openai.beta.threads.messages.list(threadId);
+    console.log(JSON.stringify(messages.data, null, 2));
+
+    res.status(200).json({ threadId, messages: messages.data });
   } catch (error) {
     console.error('Error creating the thread:', error);
     res.status(500).json({ error: 'Error creating the thread' });
