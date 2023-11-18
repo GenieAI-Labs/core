f275b2fa66d02a1798f871a2ae53c597ba21fc5b
0xRomain <romain@talentlayer.org>
2023-11-18 22:04:22 +0300

    handle submit by enter

--- dapp/src/components/Genie/GenieContent.tsx
+++ dapp/src/components/Genie/GenieContent.tsx
@@ -23,7 +23,8 @@ export default function GenieContent({ selectedGenie, onBack }: GenieContentProp
 
   const [assistant, setAssistant] = useState<any>(null);
 
-  async function postMessage() {
+  async function postMessage(e) {
+    e.preventDefault();
     setLoading(true);
     // Add user message to the conversation
     // setConversation(prevConvo => [...prevConvo, { sender: 'user', text: userInput }]);
@@ -82,26 +83,32 @@ export default function GenieContent({ selectedGenie, onBack }: GenieContentProp
         <div className='mt-4'>
           <div className='p-8 overflow-auto h-[calc(100vh-370px)] sm:h-[calc(100vh-400px)] md:h-[calc(100vh-270px)] lg:h-[calc(100vh-270px)]'>
             {conversation.map((msg, index) => (
-              <div key={index} className={`message ${msg.role === 'user' ? 'text-right' : ''}`}>
+              <div
+                key={index}
+                className={`message text-sm p-2 text-gray-900 ${
+                  msg.role === 'user' ? 'text-right' : ''
+                }`}>
                 {msg.content.map((content, index) => (
                   <p key={index}>{content.text?.value}</p>
                 ))}
               </div>
             ))}
           </div>
-          <div className='flex mt-2 p-4 relative'>
-            <input
-              type='text'
-              value={userInput}
-              onChange={e => setUserInput(e.target.value)}
-              className='border border-gray-200 rounded-xl p-2 flex-grow'
-              placeholder='Type your message here'
-            />
-            <button
-              onClick={postMessage}
-              className='bg-blue-500 text-white p-2 rounded-full absolute right-0 mr-[22px] mt-[5px]'>
-              {loading ? <FiLoader className='animate-spin' /> : <FiSend />}
-            </button>
+          <div className='flex mt-2 p-4 '>
+            <form onSubmit={postMessage} className='w-full relative'>
+              <input
+                type='text'
+                value={userInput}
+                onChange={e => setUserInput(e.target.value)}
+                className='border border-gray-200 rounded-xl p-2 w-full'
+                placeholder='Type your message here'
+              />
+              <button
+                type='submit'
+                className='bg-blue-500 text-white p-2 rounded-full absolute right-0 top-0 mr-[12px] mt-[5px]'>
+                {loading ? <FiLoader className='animate-spin' /> : <FiSend />}
+              </button>
+            </form>
           </div>
         </div>
       </div>
